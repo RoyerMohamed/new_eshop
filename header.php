2e3c3c718eed5092a4973d7534079a73d6ff6df9
marriet <benoitmarriet@gmail.com>
2024-04-22 14:44:36 +0200

    first

---
+++ header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./styles/styles.css">
+    <title>Document</title>
+</head>
+<body>
+<header>
+    <div id="hero"></div>
+    <nav>
+        <ul>
+            <li><a href="./index.php">HOME</a></li>
+            <li><a href="./about.php">ABOUT US</a></li>
+            <li><a href="./cart.php">CART</a></li>
+        </ul>
+    </nav>
+</header>
+
+<main>
